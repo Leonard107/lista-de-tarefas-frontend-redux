156a9f416e8aa13105b64f7f9646f34592b820b3
LEONARD RICHARD <leonardrichard107@gmail.com>
2021-06-21 16:45:44 -0300

    Action Creator de Pesquisa

--- src/pendencia/pendenciaForm.jsx
+++ src/pendencia/pendenciaForm.jsx
@@ -1,44 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { changeDescription } from './pendenciaActions'
+import { changeDescription, search } from './pendenciaActions'
 
-const pendenciaForm = props => {
-    const keyHandler = (e) => {
+class  pendenciaForm extends Component {
+    constructor(props) {
+        super(props)
+        this.keyHandler = this.keyHandler.bind(this)
+    }
+
+    componentWillMount() {
+        this.props.search()
+    }
+
+    keyHandler(e) {
         if(e.key === 'Enter'){
             //Se o shift tiver pressionado faça uma pesquisa, se ele não tiver pressionado adicione.
-            e.shiftKey ? props.handleSearch() : props.handleAdd()
+            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
             //Se apertar o botão Esc ele limpa o formulário
         } else if (e.key === 'Escape') {
             props.handleClear()
         }
+
     }
-    return (
-        <div role='form' className='pendenciaForm'>
-            <div className='col-xs-12 col-sm-9 col-md-10'>
-                <input id='description' className='form-control' 
-                placeholder='Adicione uma tarefa'
-                 onChange={props.changeDescription}
-                value={props.description}
-                onKeyUp={keyHandler}></input>
-            </div>
+
+    render() {
+        return (
+            <div role='form' className='pendenciaForm'>
+                <div className='col-xs-12 col-sm-9 col-md-10'>
+                    <input id='description' className='form-control' 
+                    placeholder='Adicione uma tarefa'
+                    onChange={this.props.changeDescription}
+                    value={this.props.description}
+                    onKeyUp={this.keyHandler}></input>
+                </div>
     
-            <div className='col-xs-12 col-sm-9 col-md-2'>
-                <IconButton style='primary' icon='plus'
-                    onClick={props.handleAdd}></IconButton>
-                <IconButton style='info' icon='search'
-                    onClick={props.handleSearch}></IconButton>
-                <IconButton style='default' icon='close'
-                    onClick={props.handleClear}></IconButton>
-            </div>
+                <div className='col-xs-12 col-sm-9 col-md-2'>
+                    <IconButton style='primary' icon='plus'
+                        onClick={this.props.handleAdd}></IconButton>
+                    <IconButton style='info' icon='search'
+                        onClick={this.props.handleSearch}></IconButton>
+                    <IconButton style='default' icon='close'
+                        onClick={this.props.handleClear}></IconButton>
+                </div>
         </div>
-    )
+        )
+    }
 }
 
 const mapStateToProps = state => ({description: state.pendencia.description})
 const mapDispatchToProps = dispatch => 
-    bindActionCreators({ changeDescription }, dispatch)
+    bindActionCreators({ changeDescription, search }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(pendenciaForm)
